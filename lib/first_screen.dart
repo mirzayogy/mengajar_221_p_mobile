10232053c814b6ffbc9dd50cef4e78f2a80d1609
mirzayogy <mirza.yogy@gmail.com>
2022-12-01 14:56:18 +0800

    menambahkan url_launcer, tested di iOS

--- lib/first_screen.dart
+++ lib/first_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mengajar_221_p_mobile/second_screen.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class FirstScreen extends StatelessWidget {
   static const routeName = '/first_screen';
@@ -32,9 +33,29 @@ class FirstScreen extends StatelessWidget {
               },
               child: const Text("Mirza"),
             ),
+            GestureDetector(
+              onTap: () async {
+                final Uri url = Uri.parse('https://flutter.dev');
+                if (await canLaunchUrl(url)) {
+                  await launchUrl(url);
+                } else {
+                  throw 'Could not launch $url';
+                }
+              },
+              child: const Text("Klik to Flutter Website"),
+            ),
           ],
         ),
       ),
     );
   }
+
+//   Future<void> _launchUrl(url) async {
+//     if (!await canLaunchUrl(url)) {
+//       throw 'Could not launch $url';
+//     } else {
+//       await launchUrl(url);
+//       print('ini');
+//     }
+//   }
 }
